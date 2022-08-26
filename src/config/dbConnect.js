d3ec0ef748014bf17e63bcb162460fcf056c799f
carlos.steinheuser <carlosfelipe.st28@gmail.com>
2022-08-25 20:30:01 -0400

    Atualizando o banco de dados

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.o9mrc.mongodb.net/test");
+mongoose.connect("mongodb+srv://admin:admin@cluster0.o9mrc.mongodb.net/OBSERVATORIO_POLITICAS_PUBLICAS");
 
 let db = mongoose.connection;
 
