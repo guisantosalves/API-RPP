5f3dbb8c7a2fe9f172ff15ea89c64981b56d86a7
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-10-08 17:39:39 -0400

    Gerando tags

--- src/seeds/seeds.js
+++ src/seeds/seeds.js
@@ -30,13 +30,24 @@ for (let i = 1; i <= 10; i++) {
         }
     ]
 
+    const generateTags = () => {
+        let tags = []
+
+        for (let i = 0; i < 4; i++){
+            tags.push(faker.lorem.word())
+        }
+
+        return tags
+    }
+
     const seedPublicacoes = [
         {
             titulo: faker.lorem.sentence(),
             data: faker.date.past(),
             tipo: Math.random <= 0.3 ? "Notícia" : Math.random() <= 0.6 ? "Projeto" : "Artigo",
             registro: faker.lorem.paragraphs(2),
-            usuario: seedUsuarios[0]
+            usuario: seedUsuarios[0],
+            tags: generateTags()
         }
     ]
 
